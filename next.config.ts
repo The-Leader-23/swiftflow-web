9ff57775501b3f6adc50961f0192d215bb4d234b
Katlego <letheakatlego23@gmail.com>
2025-07-30 09:30:20 +0200

    chore: force HTTPS redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "swiftflow.world",
+          },
+        ],
+        destination: "https://www.swiftflow.world/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
